7e593599e7f40329ad8b87a962e34ea667511fe7
Nitesh <niteshsinghal9@gmail.com>
2024-11-03 21:55:45 +0530

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <main className="">
+      <h1>
+        This is the starter code . Goto /try to see if the gsap and lenis are installed properly
+      </h1>
+    </main>
+  );
+}
